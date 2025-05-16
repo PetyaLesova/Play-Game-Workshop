bcc571f5f09631aa13987c1cbd53e55b3cdac223
Petya Lesova <petq_lesova@abv.bg>
2025-05-16 06:05:57 +0100

    Add Game List
    Add Navigation link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
+import { Routes, Route } from "react-router-dom"
 import Header from "./components/header/Header.jsx"
+import Home from "./components/home/Home.jsx"
+import GameList from "./components/game-list/GameList.jsx"
 
 
 function App() {
@@ -8,6 +11,13 @@ function App() {
     <div id="box">
      
       <Header/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="games" element={<GameList/>}/>
+        
+      </Routes>
+      
     </div>
   )
 }
