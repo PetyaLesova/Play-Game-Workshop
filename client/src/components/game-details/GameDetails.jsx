c83695e024ee7f026a07fb47dd885ac76d7e5aa7
Petya Lesova <petq_lesova@abv.bg>
2025-05-21 02:58:45 +0100

    Add Create comments
    Add Comments Service

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -1,27 +1,34 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import * as gameService from "../../services/gameService.js"
-import {create} from "../../services/commentService.js"
+import * as commentService from "../../services/commentService.js"
 
 export default function GameDetails(){
     const[game,setGame] = useState({})
+    const[comments,setComments] = useState([])
     const {gameId} = useParams()
 
     useEffect(() =>{
         gameService.getOne(gameId)
          .then(setGame)
+
+        commentService.getAll(gameId)
+        .then(setComments)
     }, [gameId])
+    console.log(comments);
+    
 
     const addCommentHandler = async(e)=>{
         e.preventDefault()
         const formData = new FormData(e.currentTarget)
 
-        const newComment = await create(
+        const newComment = await commentService.create(
             gameId,
             formData.get('username'),
             formData.get('comment')
         );
         console.log(newComment);
+       setComments(state=> [...state, newComment])
         
     }
 
@@ -39,31 +46,34 @@ export default function GameDetails(){
 
             <p className="text"> {game.summary}</p>
 
-            // Bonus ( for Guests and Users ) 
+            {/* // Bonus ( for Guests and Users )  */}
             <div className="details-comments">
                 <h2>Comments:</h2>
                 <ul>
-                    // //  list all comments for current game (If any) 
-                    <li className="comment">
-                        <p>Content: I rate this one quite highly.</p>
-                    </li>
-                    <li className="comment">
-                        <p>Content: The best game.</p>
-                    </li>
+                    {/* // //  list all comments for current game (If any)  */}
+                    {comments.map((comment) => (
+                         <li key={comment._id} className="comment">
+                         <p>{comment.username}: {comment.text}</p>
+                          </li>
+                    ))}
+                    
+                    
                 </ul>
-                // Display paragraph: If there are no games in the database
-                <p className="no-comment">No comments.</p>
+                {/* // Display paragraph: If there are no games in the database */}
+                {comments.length === 0 && (
+                     <p className="no-comment">No comments.</p>
+                )}
             </div>
 
-            // //  Edit/Delete buttons ( Only for creator of this game ) 
+            {/* // //  Edit/Delete buttons ( Only for creator of this game )  */}
             <div className="buttons">
                 <a href="#" className="button">Edit</a>
                 <a href="#" className="button">Delete</a>
             </div>
         </div>
 
-        // Bonus 
-        //  Add Comment ( Only for logged-in users, which is not creators of the current game ) 
+        {/* // Bonus  */}
+        {/* //  Add Comment ( Only for logged-in users, which is not creators of the current game )  */}
         <article className="create-comment">
             <label>Add new comment:</label>
             <form className="form" onSubmit={addCommentHandler}>
@@ -76,3 +86,5 @@ export default function GameDetails(){
     </section>
     )
 }
+
+//27
--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -2,6 +2,15 @@ import { request } from "../lib/request.js"
 
 const baseUrl = 'http://localhost:3030/jsonstore/comments'
 
+export const getAll = async()=>{
+    const query = new URLSearchParams({
+        where: `gameId ="${gameId}"`
+    })
+    const result = await request('GET', `${baseUrl}`)
+    //TODO: temp solution
+    return Object.values(result).filter(comment => comment.gameId === gameId)
+    
+}
 
 export const create = async (gameId, username, text) =>{
     const newComment = await request('POST', baseUrl, {gameId, username, text})
