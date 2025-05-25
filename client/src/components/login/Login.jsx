14ae6fe2f45fa6426bdbb4cdf9eb30a2bd1e11a4
Petya Lesova <petq_lesova@abv.bg>
2025-05-25 20:13:00 +0100

    Create useForm Hook
    Login form is controlled now

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,4 +1,10 @@
+import useForm from "../../hooks/useForm.jsx"
+
 export default function Login (){
+    const{values, onChange, onSubmit} =   useForm({
+        email:'',
+        password:''
+    })
     return(
         <section id="login-page" className="auth">
             <form id="login">
@@ -7,10 +13,23 @@ export default function Login (){
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com"/>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        placeholder="Sokka@gmail.com"
+                        onChange = {onChange}
+                        value={values['email']}
+                        />
 
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password"/>
+                    <input 
+                        type="password" 
+                        id="login-password" 
+                        name="password"
+                        onChange = {onChange}
+                        value={values.password}
+                        />
                     <input type="submit" className="btn submit" value="Login"/>
                     <p className="field">
                         <span>If you don't have profile click <a href="#">here</a></span>
